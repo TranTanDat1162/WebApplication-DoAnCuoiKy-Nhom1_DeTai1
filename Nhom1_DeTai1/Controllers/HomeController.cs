778e53885c70d0a43cd799cb434cd2cca63a5236
LmTrieu <lmtrieu250902@gmail.com>
2023-04-19 21:57:13 +0700

    Contact-us
    +Contact-Us page is now available (not functional)

--- Nhom1_DeTai1/Controllers/HomeController.cs
+++ Nhom1_DeTai1/Controllers/HomeController.cs
@@ -31,5 +31,13 @@ namespace Nhom1_DeTai1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
